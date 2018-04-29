5de6d5739d631b31e2798711b2ba1c850dfbaa02
enhezzz <13450737557@163.com>
2018-04-30 00:20:22 +0800

    Site updated: 2018-04-30 00:20:21

--- resume/index.js
+++
@@ -1,7 +0,0 @@
-const path = require('path');
-const express = require('express');
-const app = express();
-app.use(express.static(path.resolve(__dirname)));
-app.listen(80, function () {
-    console.log('listening...')
-})
